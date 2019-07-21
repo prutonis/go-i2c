31e48be7a55eabca78424c3c17e192ebbbca509c
Steve Conklin <steve@conklinhouse.com>
2019-07-21 16:37:03 -0500

    change improt repo

--- logger.go
+++ logger.go
@@ -1,6 +1,6 @@
 package i2c
 
-import logger "github.com/d2r2/go-logger"
+import logger "github.com/sconklin/go-logger"
 
 // You can manage verbosity of log output
 // in the package by changing last parameter value.
