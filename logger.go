3dbf3abc15203601914e57ed32daf1bb76c76841
Ion Morosanu <ion.morosanu@orange.com>
2025-01-09 13:02:15 +0200

    Move to zap logger

--- logger.go
+++ logger.go
@@ -1,10 +1,69 @@
 package i2c
 
-import logger "github.com/sconklin/go-logger"
+import (
+	"os"
+	"sync"
 
-// You can manage verbosity of log output
-// in the package by changing last parameter value.
-var lg = logger.NewPackageLogger("i2c",
-	// logger.DebugLevel,
-	logger.InfoLevel,
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
+
+var (
+	Logger *zap.Logger
+	once   sync.Once
+)
+
+type Config struct {
+	LogLevel string // "debug", "info", "warn", "error"
+	DevMode  bool   // If true, uses development config with pretty printing
+}
+
+// Initialize sets up the logger with the given configuration
+func Initialize(config Config) error {
+	var err error
+	once.Do(func() {
+		// Set the log level
+		level, err := zapcore.ParseLevel(config.LogLevel)
+		if err != nil {
+			level = zapcore.InfoLevel
+		}
+
+		// Configure encoder
+		encoderConfig := zap.NewProductionEncoderConfig()
+		encoderConfig.TimeKey = "timestamp"
+		encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+		encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+
+		// Create core
+		core := zapcore.NewCore(
+			zapcore.NewJSONEncoder(encoderConfig),
+			zapcore.AddSync(os.Stdout),
+			level,
+		)
+
+		// Create logger
+		Logger = zap.New(core)
+		if config.DevMode {
+			Logger = Logger.WithOptions(zap.Development())
+		}
+	})
+	return err
+}
+
+// GetLogger returns the configured logger instance
+func GetLogger() *zap.Logger {
+	if Logger == nil {
+		// If logger hasn't been initialized, create a default one
+		Initialize(Config{
+			LogLevel: "info",
+			DevMode:  false,
+		})
+	}
+	return Logger
+}
+
+var lg = Sugar()
+
+func Sugar() *zap.SugaredLogger {
+	return GetLogger().Sugar()
+}
